6869aef1e02ce20e2d8b199b62e63e17b211373f
Ngoc Tuan <ngoctuanitpy@gmail.com>
2020-06-03 21:56:09 +0700

    update

--- main_program.py
+++ main_program.py
@@ -9,9 +9,13 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from keras.layers import *
 from keras import layers, models, optimizers
+from keras.models import Sequential
+from sklearn.decomposition import TruncatedSVD
 import numpy as np
 import gensim
-import os 
+# Just disables the warning, doesn't enable AVX/FMA
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import io
 import pickle
 
@@ -37,10 +41,9 @@ def get_data(file_path):
     # print(X)
     return X, y
 
-
-X_data, y_data = get_data('train_nor_811.csv')
-X_test, y_test = get_data('test_nor_811.csv')
-X_val, y_val = get_data('valid_nor_811.csv')
+X_data, y_data = get_data('train.csv')
+X_test, y_test = get_data('test.csv')
+X_val, y_val = get_data('valid.csv')
 
 pickle.dump(X_data, open('Data/X_data.pkl', 'wb'))
 pickle.dump(y_data, open('Data/y_data.pkl', 'wb'))
@@ -54,65 +57,99 @@ encoder = preprocessing.LabelEncoder()
 y_data_n = encoder.fit_transform(y_data)
 y_test_n = encoder.fit_transform(y_test)
 y_val_n = encoder.fit_transform(y_val)
+# print(encoder.classes_)
+
+# WORD2VECTOR
+# word2vec_model_path = os.path.join("vi/vi.vec")
+
+# w2v = KeyedVectors.load_word2vec_format(word2vec_model_path)
+# vocab = w2v.wv.vocab
+# wv = w2v.wv
+
+# def get_word2vec_data(X):
+#     word2vec_data = []
+#     for x in X:
+#         sentence = []
+#         for word in x.split(" "):
+#             if word in vocab:
+#                 sentence.append(wv[word])
+
+#         word2vec_data.append(sentence)
+#         # print(sentence)
+#     return word2vec_data
+
+# X_data_w2v = get_word2vec_data(X_data)
+# X_test_w2v = get_word2vec_data(X_test)
+# X_val_w2v = get_word2vec_data(X_val)
 
 # TFIDF 
 # word level - we choose max number of words equal to 30000 except all words (100k+ words)
 tfidf_vect = TfidfVectorizer(analyzer='word', max_features=30000)
 tfidf_vect.fit(X_data)
-tfidf_vect.fit(X_test)
-tfidf_vect.fit(X_val)
 # learn vocabulary and idf from training set
 X_data_tfidf =  tfidf_vect.transform(X_data)
 X_test_tfidf =  tfidf_vect.transform(X_test)
 X_val_tfidf =  tfidf_vect.transform(X_val)
+
+# SVD
+svd = TruncatedSVD(n_components=300, random_state=42)
+svd.fit(X_data_tfidf)
+X_data_tfidf_svd = svd.transform(X_data_tfidf)
+X_test_tfidf_svd = svd.transform(X_test_tfidf)
+X_val_tfidf_svd = svd.transform(X_val_tfidf)
+
+f = open("RESULT/EVALUATE.txt", "w")
 
 # TRAIN MODEL
 def train_model(classifier, X_data, y_data, X_test, y_test, X_val, y_val, is_neuralnet=False, n_epochs=3):       
     # X_train, X_val, y_train, y_val = train_test_split(X_data, y_data, test_size=0.1, random_state=42)
 
     if is_neuralnet:
-        classifier.fit(X_data, y_data, validation_data=(X_val, y_val), epochs=n_epochs, batch_size=512)
+        classifier.fit(np.array(X_data), np.array(y_data), validation_data=(np.array(X_val), np.array(y_val)), epochs=n_epochs, batch_size=512)
         
         val_predictions = classifier.predict(X_val)
         test_predictions = classifier.predict(X_test)
         val_predictions = val_predictions.argmax(axis=-1)
         test_predictions = test_predictions.argmax(axis=-1)
     else:
-        classifier.fit(X_data, y_data)
+        classifier.fit(np.array(X_data), np.array(y_data))
     
         # train_predictions = classifier.predict(X_data)
         val_predictions = classifier.predict(X_val)
         test_predictions = classifier.predict(X_test)
            
-    print("Validation accuracy: ", metrics.accuracy_score(val_predictions, y_val))
-    print("Test accuracy: ", metrics.accuracy_score(test_predictions, y_test))
+    # print("Validation accuracy: ", metrics.accuracy_score(val_predictions, y_val))
+    # print("Test accuracy: ", metrics.accuracy_score(test_predictions, y_test))
+    f.write('\n- Validation accuracy: ' + str(metrics.accuracy_score(val_predictions, y_val)))
+    f.write('\n- Test accuracy: ' + str(metrics.accuracy_score(test_predictions, y_test)))
 
-print("NAIVE BAYES")
-train_model(naive_bayes.MultinomialNB(), X_data_tfidf, y_data_n, X_test_tfidf, y_test_n, X_val_tfidf, y_val_n, is_neuralnet=False)
-print("SVM")
-train_model(svm.SVC(), X_data_tfidf, y_data_n, X_test_tfidf, y_test_n, X_val_tfidf, y_val_n, is_neuralnet=False)
-print("RANDOM FOREST")
-train_model(ensemble.RandomForestClassifier(), X_data_tfidf, y_data_n, X_test_tfidf, y_test_n, X_val_tfidf, y_val_n, is_neuralnet=False)
+# LSTM
+def create_lstm_model():
+    input_layer = Input(shape=(300,))
+    layer = Reshape((10, 30))(input_layer)
+    layer = LSTM(128, activation='relu')(layer)
+    layer = Dense(512, activation='relu')(layer)
+    layer = Dense(512, activation='relu')(layer)
+    layer = Dense(128, activation='relu')(layer)
+    
+    output_layer = Dense(10, activation='softmax')(layer)
+    classifier = models.Model(input_layer, output_layer)
+    classifier.compile(optimizer=optimizers.Adam(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    return classifier
 
-# print("LINEAR REGRESSION")
-# train_model(linear_model.LogisticRegression(), X_data_tfidf, y_data, X_test_tfidf, y_test, is_neuralnet=False)
 
-# def create_lstm_model():
-#     input_layer = Input(shape=(300,))
-    
-#     layer = Reshape((10, 30))(input_layer)
-#     layer = LSTM(128, activation='relu')(layer)
-#     layer = Dense(512, activation='relu')(layer)
-#     layer = Dense(512, activation='relu')(layer)
-#     layer = Dense(128, activation='relu')(layer)
-    
-#     output_layer = Dense(10, activation='softmax')(layer)
-    
-#     classifier = models.Model(input_layer, output_layer)
-    
-#     classifier.compile(optimizer=optimizers.Adam(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-    
-#     return classifier
+f.write("[SVM + TF-IDF]")
+train_model(svm.SVC(), X_data_tfidf_svd, y_data_n, X_test_tfidf_svd, y_test_n, X_val_tfidf_svd, y_val_n, is_neuralnet=False)
 
+f.write("\n[RANDOM FOREST + TF-IDF]")
+train_model(ensemble.RandomForestClassifier(), X_data_tfidf_svd, y_data_n, X_test_tfidf_svd, y_test_n, X_val_tfidf_svd, y_val_n, is_neuralnet=False)
+
+f.write("\n[LSTM + TF-IDF]")
+classifier = create_lstm_model()
+train_model(classifier=classifier, X_data=X_data_tfidf_svd, y_data=y_data_n, X_test=X_test_tfidf_svd, y_test=y_test_n, X_val = X_val_tfidf_svd, y_val = y_val_n, is_neuralnet=True)
+
+# f.write("\n[LSTM + W2V]")
 # classifier = create_lstm_model()
-# train_model(classifier=classifier, X_data=X_data_tfidf, y_data=y_data_n, X_test=X_test_tfidf, y_test=y_test_n, is_neuralnet=True)
+# train_model(classifier=classifier, X_data=X_data_w2v, y_data=y_data_n, X_test=X_test_w2v, y_test=y_test_n, X_val = X_val_w2v, y_val = y_val_n, is_neuralnet=True)
+
+f.close()
